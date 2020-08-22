791238dd8858f3336d4d3e4e8e7b567fb06100e1
Thomas Rowntree <thomas.james.rowntree@gmail.com>
2020-08-23 00:12:04 +0930

    Add TopicObject to hold topic info

--- mqttdict/clientdict.py
+++ mqttdict/clientdict.py
@@ -1,12 +1,22 @@
+import time
 import paho.mqtt.client as mqtt
 from .exceptions import MissingPayloadError
+import collections
 
-class MqttDict():
+class TopicObject:
+     def __init__ (self,topic,payload=None,time=None):
+         self.topic = topic
+         self.payload = payload
+         self.time = time
+
+
+
+class MqttDict:
 
     def __init__(self,*args,**kwargs):
         print("init")
         
-        self.topic_payload_dict = {}
+        self.topic_object_dict = {}
         self.default_qos = 2
 
         self.mqtt_client = mqtt.Client()
@@ -19,39 +29,45 @@ class MqttDict():
 
     def on_connect_callback(self,client,userdata,flags,rc):
         #when we reconnect to the server we must subscribe to everything
-        print("connect")
-        topic_list = list(self.topic_payload_dict.keys())
-        qos_list = [self.default_qos]*len(topic_list)
+   
+        if len(self.topic_object_dict) > 0:
+            topic_list = list(self.topic_object_dict.keys())
+            qos_list = [self.default_qos]*len(topic_list)
 
-        topic_qos_list = list(zip(topic_list,qos_list))
+            topic_qos_list = list(zip(topic_list,qos_list))
 
-        self.mqtt_client.subscribe(topic_qos_list)
+            self.mqtt_client.subscribe(topic_qos_list)
+         
 
 
     def on_message_callback(self,client, userdata, msg):
-        self.topic_payload_dict[msg.topic] = msg.payload
+        self.topic_object_dict[msg.topic].payload = msg.payload
+        self.topic_object_dict[msg.topic].time = time.time()
 
     def __getitem__(self,topic):
+        
         #check if this topic has a payload
-        if topic not in self.topic_payload_dict:
+        if topic not in self.topic_object_dict:
             
-            self.topic_payload_dict[topic] = None
+            #create a new topic object for this topic
+            self.topic_object_dict[topic] = TopicObject(topic)
 
             #if the client is connected then subscribe to this topic
             if self.mqtt_client.is_connected:
                 self.mqtt_client.subscribe(topic, self.default_qos)
-            
-            raise MissingPayloadError(f"The topic '{topic}' does not have a payload yet")
 
-        if self.topic_payload_dict[topic] == None:
-            raise MissingPayloadError(f"The topic '{topic}' does not have a payload yet")
+        if self.topic_object_dict[topic].time is not None:
+            return self.topic_object_dict[topic].payload
+        
+        raise MissingPayloadError(f"The topic '{topic}' does not have a payload yet")
+        
 
-        return self.topic_payload_dict[topic]
+        
 
 
     def __setitem__(self,topic,payload):
         print("publish")
         self.mqtt_client.publish(topic,payload=payload , qos=self.default_qos, retain=True)
-        self.topic_payload_dict[topic] = payload
+        # self.topic_object_dict[topic] = TopicObject(topic,None,None)
 
 
